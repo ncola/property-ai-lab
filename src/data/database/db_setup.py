785d881d1cb353b7e0d3571f7a75626d8e318ed7
ncola <xnikola99@icloud.com>
2026-06-11 00:36:40 +0200

    update database configuration for Neon PostgreSQL

--- src/data/database/db_setup.py
+++ src/data/database/db_setup.py
@@ -15,8 +15,9 @@ DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_HOST = os.getenv("DB_HOST", "localhost")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME")
+DB_SSLMODE = os.getenv("DB_SSLMODE", "disable")
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode={DB_SSLMODE}"
 
 class Base(DeclarativeBase):
     pass
